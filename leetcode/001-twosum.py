052ff34a9b5283f813cc11fc77f0339c98446a73
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-07-21 22:08:34 -0500

    O(n^2) and O(n) solution - twosum

--- leetcode/001-twosum.py
+++ leetcode/001-twosum.py
@@ -42,26 +42,18 @@ nums4 = [0,4,3,0]
 target4 = 0
 print(two_sum(nums4, target4))
 
-##issue! if you cleaned the number array the indices changes! (see num4 and target4, expected answer is [0,3])!!!
+##issue! ⬆️⬆️⬆️⬆️ if you cleaned the number array the indices changes! (see num4 and target4, expected answer is [0,3])!!!
 
-#improved brute force solution (O(n^2))
+#working brute force solution (O(n^2)) ⬇️⬇️⬇️
 def two_sum(nums, target):
-    cleaned_num = {}
-    for index, num in enumerate(nums):
-        if num <= target:
-            cleaned_num[num] = index
-    keys_list = list(cleaned_num.keys())
-    print('cleanednum:', cleaned_num)
-    print('keys_list', keys_list)
-    answer = []
-    for i in range(0, len(keys_list)):
-        for j in range(i+1, len(keys_list)):
-            if keys_list[i] + keys_list[j] == target:
-                answer.append(cleaned_num[keys_list[i]])
-                answer.append(cleaned_num[keys_list[j]])
-        
+    print("working two sum solution")
+    answer=[]
+    for i in range(0,len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                answer.append(i)
+                answer.append(j)
     return answer
-
 #tests
 nums1 = [2,7,11,15]
 target1 = 9
@@ -78,3 +70,51 @@ print(two_sum(nums3, target3))
 nums4 = [0,4,3,0]
 target4 = 0
 print(two_sum(nums4, target4))
+
+
+#solution less than O(n^2)
+#approach - use a dictionary to map values to indices (in a list)
+#go through the nums list and find the complement needed to get up to the target number. 
+def optimized_two_sum(nums, target):
+    nums_dict = {}
+    for index, num in enumerate(nums):
+        if num in nums_dict:
+            nums_dict[num].append(index)
+        else:
+            nums_dict[num] = [index]
+    
+    print("optimized:", nums_dict)
+    answer = []
+    for num in nums:
+        complement = target - num
+        print("complement:", complement, " num:", num)
+        print("length:", len(nums_dict[num]))
+        if len(nums_dict[num]) < 2:
+            if complement!=num and complement in nums_dict:
+                answer.append(nums_dict[num][0])
+                answer.append(nums_dict[complement][0])
+                break
+        elif len(nums_dict[num]) >=2:
+            if complement == num:
+                answer.append(nums_dict[num][0])
+                answer.append(nums_dict[num][1])
+                break
+            
+    return answer
+        
+#tests
+nums1 = [2,7,11,15]
+target1 = 9
+print(optimized_two_sum(nums1, target1))
+
+nums2 = [3,2,4]
+target2 = 6
+print(optimized_two_sum(nums2, target2))
+
+nums3 = [3,3]
+target3 = 6
+print(optimized_two_sum(nums3, target3))
+
+nums4 = [0,4,3,0]
+target4 = 0
+print(optimized_two_sum(nums4, target4))

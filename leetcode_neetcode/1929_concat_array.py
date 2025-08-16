178f10bb644c0502cb8055cb72db5824fe85d10d
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-15 20:39:35 -0500

    1929 - concatenation array

---
+++ leetcode_neetcode/1929_concat_array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = nums.concat(nums)
+        return ans

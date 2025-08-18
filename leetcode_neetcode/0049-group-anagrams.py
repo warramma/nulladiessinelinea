c5ec0ac9452d7d7ebb18d01e151c8fa5a5ad0d3f
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-17 23:18:18 -0500

    49 - group anagram letter fequency as key

---
+++ leetcode_neetcode/0049-group-anagrams.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        frequency_dict = {}
+
+        for item in strs:
+            letter_count = [0] * 26
+            for char in item:
+                letter_count[ord(char)-ord("a")] += 1
+
+            letter_tuple = tuple(letter_count)
+
+            if letter_tuple in frequency_dict:
+                frequency_dict[letter_tuple].append(item)
+            else:
+                frequency_dict[letter_tuple] = [item]
+            
+
+        return list(frequency_dict.values())
+
+        
+        
+        
+        anagram_list = [x for x in anagram_dict]
+        

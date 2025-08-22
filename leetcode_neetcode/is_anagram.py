0f9db948d794cbc5a3eb004e4e61307af89034a9
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-21 21:41:08 -0500

    is anagram

---
+++ leetcode_neetcode/is_anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        t_dict = {}
+
+        for char in s:
+            if char in s_dict:
+                s_dict[char] += 1
+            else:
+                s_dict[char] = 1
+        for char in t:
+            if char in t_dict:
+                t_dict[char] += 1
+            else:
+                t_dict[char] = 1
+        
+        return s_dict == t_dict

fd325ed0509554e0374fc4805a0c2763d32369e8
warramma | Wardiyah <wrammazy@mail.bradley.edu>
2025-08-21 21:38:55 -0500

    alternate solution to contains_duplicate

--- leetcode_neetcode/contains_duplicate.py
+++ leetcode_neetcode/contains_duplicate.py
@@ -9,3 +9,16 @@ def hasDuplicate(self, nums: List[int]) -> bool:
                 result = True
             else:
                 my_set.add(num)
+
+# alternate solution also O(n) but uses a frequency dictionary instead
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        freq_dict = {}
+
+        for num in nums:
+            if num in freq_dict:
+                return True
+            else:
+                freq_dict[num] = 1
+        
+        return False
